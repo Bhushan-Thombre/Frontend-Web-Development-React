fc91ca8be87efd7d1427c57308c48de1607613b3
Bhushan Thombre <bhushanthombre2001@gmail.com>
2022-01-16 11:09:54 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -26,17 +26,17 @@ class CommentForm extends Component {
 
     }
 
-    handleCommentFormSubmit(values) {
-        console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
-
-
-    }
+   
 
     toggleCommentFormModal() {
         this.setState({
             isCommentFormModalOpen: !this.state.isCommentFormModalOpen
         });
+    }
+
+    handleCommentFormSubmit(values) {
+        this.toggleCommentFormModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
 
@@ -186,7 +186,7 @@ class CommentForm extends Component {
         }
     }
 
-    function RenderComments({dish,comments}){
+    function RenderComments({comments, addComment, dishId}){
         if (comments == null) {
             return (<div></div>)
         }
@@ -213,7 +213,7 @@ class CommentForm extends Component {
                 <ul className='list-unstyled'>
                     {cmnts}
                 </ul>
-                <CommentForm dish={dish} comments={comments} />
+                <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         )
     }
@@ -248,7 +248,7 @@ class CommentForm extends Component {
 
                 <div className='row'>
                     <RenderDish dish={ props.dish } />
-                    <RenderComments dish={props.dish} comments={ props.comments } />
+                    <RenderComments comments={ props.comments } addComment={props.addComment} dishId={props.dish.id} />
                 </div>
 
 
